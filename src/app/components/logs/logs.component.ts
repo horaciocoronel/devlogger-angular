c5ea6e7345dac54c68afde32bb62a3942b07123f
horaciocoronel <arielc@gmail.com>
2018-04-01 18:48:31 -0400

    Import Log model to logs.component

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Log } from '../../models/Log';
 
 @Component({
   selector: 'app-logs',
@@ -6,11 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-	logs: {
-		id: string,
-		text: string,
-		date: any
-	}[];
+	logs: Log[];
   constructor() { }
 
   ngOnInit() {
