5c540dffa60e0c9120734c5372e0e3d6ffe34e3c
horaciocoronel <arielc@gmail.com>
2018-04-01 18:28:36 -0400

    Add logs property and define initial data in ngOninit

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
-
+	logs: {
+		id: string,
+		text: string,
+		date: any
+	}[];
   constructor() { }
 
   ngOnInit() {
+		this.logs = [
+			{
+				id: '1',
+				text: 'Generated components',
+				date: new Date( 3/31/2018)
+			},
+			{
+				id: '2',
+				text: 'Added Bootstrap',
+				date: new Date( 4/1/2018)
+			},
+			{
+				id: '3',
+				text: 'Added logs component',
+				date: new Date( 4/1/2018)
+			}
+		]
   }
 
 }
